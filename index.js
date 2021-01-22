f296285569f8f30b35b8dea757da971f924f0561
JangWoohyun <wkddngus5@naver.com>
2021-01-22 09:49:06 +0900

    feat: connect firebase

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Would you');
+window.onload = () => {
+  const config = {
+    apiKey: 'AIzaSyBUdxtCdqd6xg52FMKFuHN6Die3ixhf_R8',
+    authDomain: 'wouldyou-ee230.firebaseapp.com',
+    databaseURL: 'https://wouldyou-ee230-default-rtdb.firebaseio.com/',
+    storageBucket: 'wouldyou-ee230.appspot.com',
+  };
+
+  firebase.initializeApp(config);
+  const database = firebase.database();
+
+  console.log(database);
+};
