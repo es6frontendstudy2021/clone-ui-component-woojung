3fc5c4ae036f369257be8ed47b99f19cf4e56497
jexists <jexists486@gmail.com>
2021-01-23 21:18:44 +0900

    init map

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { authService, databaseService } from './firebase';
 import './style.scss';
 
+let map = null;
+
 window.onload = () => {
   function addPlace({ name, address }) {
     databaseService.ref('places/1').set({
@@ -8,4 +10,17 @@ window.onload = () => {
       address,
     });
   }
+
+  function initMap() {
+    const mapOptions = {
+      center: new naver.maps.LatLng(37.5666805, 126.9784147),
+      zoom: 15,
+      mapTypeId: naver.maps.MapTypeId.NORMAL
+    };
+
+    map = new naver.maps.Map('map', mapOptions);
+  }
+
+  initMap();
+
 };
