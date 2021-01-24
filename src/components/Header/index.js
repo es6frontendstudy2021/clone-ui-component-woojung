e313ee145121bdac3c3f246174db68f8c08722fc
JangWoohyun <wkddngus5@naver.com>
2021-01-24 16:07:30 +0900

    feat: componentize Header, Button, Modal

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,3 +1,34 @@
-const Header = () => {};
+import Button, { BUTTON_COLOR } from '../Button';
+import { showModal } from '../common';
+
+const Header = ({ className = '' } = {}) => {
+  const onClickLogin = ({ target }) => {
+    showModal({ target, modalSelector: '#modal' });
+  };
+
+  return `
+    <header class=${className}>
+      <nav class="navbar navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="/"></a>
+          <div class="d-flex header__right">
+            ${Button({
+              className: 'header__signin',
+              color: BUTTON_COLOR.YELLOW,
+              content: '로그인',
+              onClick: onClickLogin,
+            })}
+            <button
+              type="submit"
+              class="btn btn-outline-warning header__signup"
+              data-bs-toggle="modal" data-bs-target="#modal">
+              회원가입
+            </button>
+          </div>
+        </div>
+      </nav>
+    </header>
+  `;
+};
 
 export default Header;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import { authService, databaseService } from './firebase';
+import Header from './components/Header';
+import Button from './components/Button';
+import Modal from './components/Modal';
 import './style.scss';
 
 let map = null;
 
 window.onload = () => {
+  const $root = document.querySelector('#root');
+  $root.innerHTML = `
+    ${Header({ className: 'header' })}
+    ${Modal({ id: 'modal' })}
+  `;
+
   function addPlace({ name, address }) {
     databaseService.ref('places/1').set({
       name,
@@ -15,12 +24,11 @@ window.onload = () => {
     const mapOptions = {
       center: new naver.maps.LatLng(37.5666805, 126.9784147),
       zoom: 15,
-      mapTypeId: naver.maps.MapTypeId.NORMAL
+      mapTypeId: naver.maps.MapTypeId.NORMAL,
     };
 
     map = new naver.maps.Map('map', mapOptions);
   }
 
   initMap();
-
 };
