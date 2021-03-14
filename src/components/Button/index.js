e0b4b980f1e097cc356e3b4f91f4d1adf156fc8e
JangWoohyun <wkddngus5@naver.com>
2021-03-14 12:49:17 +0900

    feat: publish button component

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,7 +1,45 @@
 import { getUniqueId } from "../../../lib/id";
 import { setComponentAttributes } from "../../../lib/react";
+import './style.scss';
 
-const Button = ({ children, onClick = () => {} }) => {
+const TYPE_CLASS_MAP = {
+  primary: 'ant-btn-primary' ,
+  default: '',
+  dashed: 'ant-btn-dashed',
+  text: 'ant-btn-text',
+  link: 'ant-btn-link',
+};
+
+const SIZE_CLASS_MAP = {
+  large: 'ant-btn-lg' ,
+  middle: '',
+  small: 'ant-btn-sm' ,
+};
+
+const getClass = ({ type, size, danger }) => {
+  const classList = [];
+  classList.push(TYPE_CLASS_MAP[type]);
+  classList.push(SIZE_CLASS_MAP[size]);
+  if (danger) classList.push('ant-btn-dangerous');
+  return classList.join(' ');
+};
+
+const Button = ({
+  children,
+  block = false,
+  danger = false,
+  disabled = false,
+  ghost = false,
+  href = '',
+  htmlType = 'button',
+  icon = null,
+  loading = false,
+  shpe,
+  size = 'middle',
+  target = '',
+  type = 'primary',
+  onClick = event => { console.log(event) },
+}) => {
   const reactId = getUniqueId();
 
   setComponentAttributes({
@@ -9,9 +47,11 @@ const Button = ({ children, onClick = () => {} }) => {
     attributes: {onclick: () => onClick()},
   });
 
+  
+
   return `
-    <button class="Button" data-reactid="${reactId}">
-      ${children}
+    <button class="ant-btn ${getClass({ type, size, danger })}" data-reactid="${reactId}">
+      <span>${children}</span>
     </button>
   `;
 };
